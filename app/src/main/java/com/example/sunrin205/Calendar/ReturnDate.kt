838851442970563d72d8ea31acfaf1dd38359139
samgashyeong <samgashyeong@naver.com>
2021-08-17 23:58:54 +0900

    시간표 레이아웃 오류

--- app/src/main/java/com/example/sunrin205/Calendar/ReturnDate.kt
+++ app/src/main/java/com/example/sunrin205/Calendar/ReturnDate.kt
@@ -35,9 +35,15 @@ class ReturnDate {
         //first day of week
         cal[Calendar.DAY_OF_WEEK] = 1
 
-        val year1 = cal[Calendar.YEAR]
-        val month1 = cal[Calendar.MONTH] + 1
-        val day1 = cal[Calendar.DAY_OF_MONTH]
+        val year1 = cal[Calendar.YEAR].toString()
+        var month1 = cal[Calendar.MONTH].plus(1).toString()
+        var day1 = cal[Calendar.DAY_OF_MONTH].toString()
+        if(month1.toInt()<10){
+            month1 = "0$month1"
+        }
+        if(day1.toInt()<10){
+            day1 = "0$day1"
+        }
         list.add("${year1}${month1}${day1}")
 
         //last day of week
@@ -46,8 +52,14 @@ class ReturnDate {
         cal[Calendar.DAY_OF_WEEK] = 7
 
         val year7 = cal[Calendar.YEAR]
-        val month7 = cal[Calendar.MONTH] + 1
-        val day7 = cal[Calendar.DAY_OF_MONTH]
+        var month7 = cal[Calendar.MONTH].plus(1).toString()
+        var day7 = cal[Calendar.DAY_OF_MONTH].plus(1).toString()
+        if(month7.toInt()<10){
+            month7 = "0$month7"
+        }
+        if(day1.toInt()<10){
+            day7 = "0$day7"
+        }
         list.add("${year7}${month7}${day7}")
 
         return list
